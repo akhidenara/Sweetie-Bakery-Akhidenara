b8a5a95d14013771d3ff9c802f6486df65344a40
akhidenara <akhidenarans@gmail.com>
2026-08-15 12:01:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function hitungTotal() {
-    alert("Tombol berhasil bekerja!");
+    let produk = document.getElementById("produk").value;
+    let jumlah = document.getElementById("jumlah").value;
+
+    let harga = 0;
+
+    if (produk === "Chocolate Cake") {
+        harga = 85000;
+    } else if (produk === "Strawberry Cake") {
+        harga = 90000;
+    } else if (produk === "Butter Cookies") {
+        harga = 45000;
+    }
+
+    let total = harga * jumlah;
+
+    alert("Total pesanan kamu adalah Rp " + total.toLocaleString("id-ID"));
 }
